6339982987f9170b3d69d0b920ce2449d6ce0802
Oleksandr Pelykh <Oleksandr.Pelykh@playtech.com>
2017-09-08 08:25:59 +0300

    1st commit

---
+++ src/com/javarush/test/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.test;
+
+/**
+ * Created by oleksandrpe on 08-Sep-17.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
